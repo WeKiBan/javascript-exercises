926d8d63e0513107f963645b308fa22f39b157ca
Wekiban <wes.banyard@googlemail.com>
2020-03-31 16:06:40 +0200

    completed removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
 const removeFromArray = function() {
-
+    let array = arguments[0];
+    for(var i = 1; i< arguments.length; i++){
+        if(array.includes(arguments[i])){
+            array.splice(array.indexOf(arguments[i]), 1);
+        }
+    }
+    return array;
 }
-
+removeFromArray([1, 2, 3, 4], 3);
 module.exports = removeFromArray
