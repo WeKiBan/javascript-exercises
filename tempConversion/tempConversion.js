eded6cbdaad560eb0e4fe80165c0d63800248ff5
Wekiban <wes.banyard@googlemail.com>
2020-03-31 18:27:55 +0200

    completed tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,22 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  let result = ((num - 32) * 5/9);
+  if(Number.isInteger(result)) return result;
+  return parseFloat(result.toFixed(1));
+  
+  
 }
 
-const ctof = function() {
-
+const ctof = function(num) {
+  let result = (num * 9/5 + 32);
+  if(Number.isInteger(result)) return result;
+  return parseFloat(result.toFixed(1));
+  
 }
+
+
 
 module.exports = {
   ftoc,
   ctof
 }
+
