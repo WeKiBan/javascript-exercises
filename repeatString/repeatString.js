9ba04638b8bef5b3eb75ff08e4c97dd51a224839
Wekiban <wes.banyard@googlemail.com>
2020-03-31 14:51:48 +0200

    completed helloWorld and repeatString exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeatTimes) {
+    let string = "";
+    if(repeatTimes < 0){
+        return "ERROR";
+    } else {
+        for(var i = 0; i < repeatTimes; i++){
+            string += str;
+        }
+    }
+    return string;
 }
 
 module.exports = repeatString
