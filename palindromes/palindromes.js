c54a86c37e224c8fad50999f410af9cd2c9de447
Wekiban <wes.banyard@googlemail.com>
2020-04-09 15:45:34 +0200

    finished palindrome challenge

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function() {
+const palindromes = function(phrase) {
 
+    return phrase.replace(/\W/g, '').toLowerCase().split('').reverse().join('') === phrase.replace(/\W/g, '').toLowerCase() ? true : false;
 }
 
 module.exports = palindromes
