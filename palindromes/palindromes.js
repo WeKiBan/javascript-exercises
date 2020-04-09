804a5d61890428673bd99c66f5a19a5a9bed34a9
Wekiban <wes.banyard@googlemail.com>
2020-04-09 16:01:56 +0200

    removed unnecessary if function

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,6 +1,6 @@
 const palindromes = function(phrase) {
 
-    return phrase.replace(/\W/g, '').toLowerCase().split('').reverse().join('') === phrase.replace(/\W/g, '').toLowerCase() ? true : false;
+    return phrase.replace(/\W/g, '').toLowerCase().split('').reverse().join('') === phrase.replace(/\W/g, '').toLowerCase();
 }
 
-module.exports = palindromes
+module.exports = palindromes;
