fffe48ff9e8258b915543b3a0d771118a90b145a
Wekiban <wes.banyard@googlemail.com>
2020-04-10 14:51:33 +0200

    caeser challenge completed

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,38 @@
-const caesar = function() {
+const caesar = function (str, shift) {
+    let splitString = str.split('');
+    let newArr = [];
+    let shiftCalc = shift > 26 || shift < -26 ? shift % 26 : shift;
+    let alpha = /[a-zA-Z]/;
 
+
+    splitString.forEach(element => {
+        let newChar;
+        let wasUpperCase;
+        
+        if (element.toLowerCase() !== element) {
+            wasUpperCase = true;
+        }
+
+        if (!element.match(alpha)) {
+            return newArr.push(element);
+        } else if ((element.toLowerCase().charCodeAt(0) + shiftCalc) <= 122 && (element.toLowerCase().charCodeAt(0) + shiftCalc) >= 97) {
+            newChar = element.toLowerCase().charCodeAt(0) + shiftCalc;
+
+
+        } else if ((element.toLowerCase().charCodeAt(0) + shiftCalc) > 122) {
+            newChar = (element.toLowerCase().charCodeAt(0) + shiftCalc) - 26;
+
+        } else {
+            newChar = (element.toLowerCase().charCodeAt(0) + shiftCalc) + 26;
+
+        }
+
+        wasUpperCase ? newArr.push(String.fromCharCode(newChar).toUpperCase()) : newArr.push(String.fromCharCode(newChar));
+
+    });
+
+    return newArr.join('');
 }
+caesar('Hello, World!', 75)
 
-module.exports = caesar
+module.exports = caesar;
