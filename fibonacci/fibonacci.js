19189713786b51f53e5d8d3c02e09d24d49f500c
Wekiban <wes.banyard@googlemail.com>
2020-04-10 15:39:17 +0200

    completed fibonacci challenge

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+let fibArr = [0, 1];
 
+if(number < 0) return "OOPS";
+
+
+for(var i = 1; i < number; i++){
+ let nextNum = fibArr[i] + fibArr[i-1];
+    fibArr.push(nextNum);
+ }
+
+ return fibArr[number];
 }
+
+fibonacci(0);
+
 
 module.exports = fibonacci
